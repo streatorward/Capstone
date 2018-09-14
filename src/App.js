97729df40b8b5276de85e57e72e11828a7737a30
streatorward <streatorward@gmail.com>
2018-09-14 14:45:22 -0500

    logo added, sorting items by type finished and come buttons populated with images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="slackerz.png" className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Capstone</h1>
         </header>
       </div>
--- src/components/task/TaskList.js
+++ src/components/task/TaskList.js
@@ -1,13 +1,18 @@
 // import { Link } from 'react-router-dom'
 import React, { Component } from 'react'
 import TaskCard from './TaskCard'
-// import "./Task.css"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./taskCard.css"
+import "./taskList.css"
 
 
 export default class TaskList extends Component {
 
     render() {
         const filteredTasks = this.props.tasks.filter(task => task.complete !== true)
+        const dailyTasks = filteredTasks.filter(task => task.taskRecurring === "daily")
+        const weeklyTasks = filteredTasks.filter(task => task.taskRecurring === "weekly")
+        const monthlyTasks = filteredTasks.filter(task => task.taskRecurring === "monthly")
         return (
 
             <React.Fragment>
@@ -20,11 +25,29 @@ export default class TaskList extends Component {
                         }>
                         Add task
                     </button>
-                    {
-                            filteredTasks.map(task =>
-                                <TaskCard key={task.id} task={task} {...this.props}/>
-                            )
-                        }
+                    <div className="allTasks">
+                        <div className="taskColumn">
+                            {
+                                dailyTasks.map(task =>
+                                    <TaskCard key={task.id} task={task} {...this.props} />
+                                )
+                            }
+                        </div>
+                        <div className="taskColumn">
+                            {
+                                weeklyTasks.map(task =>
+                                    <TaskCard key={task.id} task={task} {...this.props} />
+                                )
+                            }
+                        </div>
+                        <div className="taskColumn">
+                            {
+                                monthlyTasks.map(task =>
+                                    <TaskCard key={task.id} task={task} {...this.props} />
+                                )
+                            }
+                        </div>
+                    </div>
                 </div>
             </React.Fragment>
         )
